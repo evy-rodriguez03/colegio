af771f077aafaa2edd4e4aadf0e5c0b95b570110
Alejandro <102131329+AlejandroMendoz@users.noreply.github.com>
2023-02-12 23:04:56 -0600

    Arreglos en dashboard, creación vista Usuario

---
+++ resources/views/Administracion/Usuarios/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layout.app')
+
+
+@section('content')
+<br><br><br><br>
+<div class="card shadow">
+    <div class="card-header border-0">
+      <div class="row align-items-center">
+        <div class="col">
+          <h3 class="mb-0">Usuarios</h3>
+        </div>
+        <div class="col text-right">
+          <a href="{{url('/usuarios/crear')}}" class="btn btn-sm btn-primary">Nuevo Usuario</a>
+        </div>
+      </div>
+    </div>
+    <div class="table-responsive">
+      <!-- Projects table -->
+      <table class="table align-items-center table-flush">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Correo</th>
+            <th scope="col">Contraseña</th>
+            <th scope="col">Rol</th>
+            <th scope="col">Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">
+              /argon/
+            </th>
+            <td>
+              4,569
+            </td>
+            <td>
+              340
+            </td>
+           <td>
+
+           </td>
+           <td>
+            <a href="" class="btn btn-sm bt-primary">Editar</a>
+            <a href="" class="btn btn-sm bt-danger">Eliminar</a>
+           </td>
+       
+        </tbody>
+      </table>
+    </div>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\controllers\RegisterController;
 use App\Http\controllers\SessionsController;
+use App\Http\controllers\UsuarioController;
 
 
 Route::get('/', function () {
-    return view('Home');
+    return view('dashboard');
 });
 
 /*Ruta de Login */
@@ -24,5 +25,11 @@ Route::get('/register', [RegisterController::class,
  Route::post('/register', [RegisterController::class,
  'store'])->name('register.index');
 
+//rutas usuario
 
+route::get('/usuarios', [UsuarioController::Class,'index'])->name('usuarios.index');
+
+route::get('/usuarios/crear', [UsuarioController::Class,'create'])->name('usuarios.create');
+route::get('/usuarios/{usuario}/edit', [UsuarioController::Class,'edit'])->name('usuarios.edit');
+route::post('/usuarios', [UsuarioController::Class,'sendData']);
 
