8c3e7dec835bd810f95820eee6aa612bc01eba58
evy-rodriguez03 <evyrodriguez.@gmail.com>
2023-02-12 23:24:36 -0600

    Roles Asignados

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role1 = Role::create(['name' => 'Admin']);
+        $role2 = Role::create(['name' => 'Secretaria']);
+        $role3 = Role::create(['name' => 'Tesoreria']);
+        $role4 = Role::create(['name' => 'Orientacion']);
+
+        Permission::create(['name' => 'admin.dashboard.index']);
+         
+        $role1->permissions()->attach(1);
+    
+    }
+}
