1624411c5d5f3b2700c07f87fe53b8e48e7c137c
evy-rodriguez03 <evyrodriguez.@gmail.com>
2023-02-16 00:55:48 -0600

    inicio y cierre de matricula

---
+++ resources/views/Administracion/prinperiodo.blade.php
@@ -0,0 +1,29 @@
+
+@extends('layout.panel')
+
+@section('content')
+
+<div>
+  <div class="row row-cols-1 row-cols-md-4 py-8 g-12">
+    <div class="col">
+      <div class="card">
+      <center><img src="{{asset('img/brand/inicio.png') }}" class="card-img-top" alt="..." style="width:210px;height:200px;"></center>
+        <div class="card-body">
+          <center><a href="{{Route('inicio')}}" class="btn btn-lg btn-info">Inicio de Matricula </a></center>
+          
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+      <center><img src="{{asset('img/brand/cierre.png') }}" class="card-img-top" alt="..." style="width:190px;height:200px;"></center>
+ <div class="card-body">
+          <center><a href="{{url('cierre')}}" class="btn btn-lg btn-info">Cierre de Matricula</a></center>
+        </div>
+      </div>
+    </div>
+
+  
+
+
+@endSection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use App\Http\controllers\SessionsController;
 use App\Http\controllers\DashboardController;
 use App\Http\controllers\UsuarioController;
 use App\Http\controllers\dashboardsecController;
+use App\Http\controllers\PeriodomController;
+use App\Http\controllers\IniciomController;
+use App\Http\controllers\FinalizarController;
+use App\Http\controllers\ControllerEvent;
+
 
 
 /*Ruta de Login */
@@ -23,8 +28,25 @@ Route::get('/dashboardsec', [dashboardsecController::class,
 'create'])->name('dashboardsec.index');
 
 //rutas usuario
-route::get('/usuarios', [UsuarioController::Class,'index'])->name('usuarios.index');
-route::get('/usuarios/crear', [UsuarioController::Class,'create'])->name('usuarios.create');
-route::get('/usuarios/{usuario}/edit', [UsuarioController::Class,'edit'])->name('usuarios.edit');
-route::post('/usuarios', [UsuarioController::Class,'sendData']);
+route::get('/usuarios', [UsuarioController::class,'index'])->name('usuarios.index');
+route::get('/usuarios/crear', [UsuarioController::class,'create'])->name('usuarios.create');
+route::get('/usuarios/{usuario}/edit', [UsuarioController::class,'edit'])->name('usuarios.edit');
+route::post('/usuarios', [UsuarioController::class,'sendData']);
 
+/*Rutas inicio y cieree de matricula */
+Route::get('/prinperiodo', [PeriodomController::class, 
+'create'])->name('periodo');
+
+Route::get('/iniciom', [IniciomController::class, 
+'create'])->name('inicio');
+
+Route::get('/cierrem', [FinalizarController::class, 
+'create'])->name('cierre');
+
+/*(Calendario)*/
+Route::get('evento/form','ControllerEvent@form')->name('evento.index');
+Route::post('evento/create','ControllerEvent@create');
+Route::get('evento/details/{id}','ControllerEvent@details');
+Route::get('evento/index','ControllerEvent@index');
+Route::get('evento/index/{month}','ControllerEvent@index_month');
+Route::post('evento/calendario','ControllerEvent@calendario');
