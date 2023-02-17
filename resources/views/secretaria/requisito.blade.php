819b4759b51a47a84b972c3723b6948ff1aefeba
Allan Escoto <aeescoto@unah.hn>
2023-02-16 20:35:24 -0600

    Formularios Secretaria (2)

--- resources/views/secretaria/requisito.blade.php
+++ resources/views/secretaria/requisito.blade.php
@@ -1,76 +1,89 @@
-@extends('layout.app')
+@extends('layout.panel')
 
 @section('content')
 
-    <div class="container pt-8 ">
-    <h1 class="text">Requisitos</h1>
+    <div class="card shadow">
+        <div class="card-header border-0">
+            <div class="row align-items-center">
+            <div class="col">
+                <h1 class="mb-0">Requisitos</h1>
+            </div>
+            <div class="col text-right">
+            <a href="{{url('#')}}" class="btn btn-sm btn-success">
+                <i class="fas fa-angle-left"></i>
+                Regresar</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="card-body">
     <h2 class="text">Datos Recibidos</h2>
 
     <form class="row g-3 mt-3">
-        <div class="col-6 mt-3">
+        <div class="col-4 mt-3">
             <label for="name"> </label>
         </div>
-        <div class="col-3 mt-3">
+        <div class="col-4 mt-3">
             <label for="name">Recibido</label>
         </div>
-        <div class="col-3 mt-3">
+        <div class="col-4 mt-3">
             <label for="name">Pendiente</label>
         </div>
-        <div class="col-6 mt-3">
-            <label for="name">Partida de Nacimiento</label>
+        <div class="col-4 mt-3">
+            <label for="name">Partida de Nacimiento:</label>
         </div>
-        <div class="col-3 mt-3">
-            <input type="text" id="nacimientoR" name="nacimientoR"></input>
+        <div class="col-4 mt-3">
+            <input type="text" id="nacimientoR" name="nacimientoR" class="form-control"></input>
         </div>
-        <div class="col-3 mt-3">
-            <input type="text" id="nacimientoP" name="nacimientoP"></input>
-        </div>
-
-        <div class="col-6 mt-3">
-            <label for="name">Fotografia</label>
-        </div>
-        <div class="col-3 mt-3">
-            <input type="text" id="fotografiaR" name="fotografiaR"></input>
-        </div>
-        <div class="col-3 mt-3">
-            <input type="text" id="fotografiaP" name="fotografiaP"></input>
+        <div class="col-4 mt-3">
+            <input type="text" id="nacimientoP" name="nacimientoP" class="form-control"></input>
         </div>
 
-        <div class="col-6 mt-3">
-            <label for="name">Fotografia Padre</label>
+        <div class="col-4 mt-3">
+            <label for="name">Fotografía:</label>
         </div>
-        <div class="col-3 mt-3">
-            <input type="text" id="fotografiaPadreR" name="fotografiaPadreR"></input>
+        <div class="col-4 mt-3">
+            <input type="text" id="fotografiaR" name="fotografiaR" class="form-control"></input>
         </div>
-        <div class="col-3 mt-3">
-            <input type="text" id="FotografiaPadreP" name="FotografiaPadreP"></input>
-        </div>
-
-        <div class="col-6 mt-3">
-            <label for="name">Fotografia Carnet Vacuna</label>
-        </div>
-        <div class="col-3 mt-3">
-            <input type="text" id="carnetR" name="carnetR"></input>
-        </div>
-        <div class="col-3 mt-3">
-            <input type="text" id="carnetP" name="carnetP"></input>
+        <div class="col-4 mt-3">
+            <input type="text" id="fotografiaP" name="fotografiaP" class="form-control"></input>
         </div>
 
-        <div class="col-6 mt-3">
-            <label for="name">Certificado de Conducta</label>
+        <div class="col-4 mt-3">
+            <label for="name">Fotografía Padre:</label>
         </div>
-        <div class="col-3 mt-3">
-            <input type="text" id="conductaR" name="conductaR"></input>
+        <div class="col-4 mt-3">
+            <input type="text" id="fotografiaPadreR" name="fotografiaPadreR" class="form-control"></input>
         </div>
-        <div class="col-3 mt-3">
-            <input type="text" id="conductaP" name="conductaP"></input>
+        <div class="col-4 mt-3">
+            <input type="text" id="FotografiaPadreP" name="FotografiaPadreP" class="form-control"></input>
         </div>
 
-        <div class="col-6 mt-3">
-            <label for="name">Total Matricula</label>
+        <div class="col-4 mt-3">
+            <label for="name">Fotografía Carnet Vacuna:</label>
         </div>
-        <div class="col-6 mt-3">
-            <input type="text" id="total" name="total"></input>
+        <div class="col-4 mt-3">
+            <input type="text" id="carnetR" name="carnetR" class="form-control"></input>
+        </div>
+        <div class="col-4 mt-3">
+            <input type="text" id="carnetP" name="carnetP" class="form-control"></input>
+        </div>
+
+        <div class="col-4 mt-3">
+            <label for="name">Certificado de Conducta:</label>
+        </div>
+        <div class="col-4 mt-3">
+            <input type="text" id="conductaR" name="conductaR" class="form-control"></input>
+        </div>
+        <div class="col-4 mt-3">
+            <input type="text" id="conductaP" name="conductaP" class="form-control"></input>
+        </div>
+
+        <div class="col-4 mt-3">
+            <label for="name">Total Matrícula:</label>
+        </div>
+        <div class="col-8 mt-3">
+            <input type="text" id="total" name="total" class="form-control"></input>
         </div>
 
         <button type="button" class="btn btn-primary mt-3">
