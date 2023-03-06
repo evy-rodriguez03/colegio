e22495258f9a26945f6b2dc1eaad52e838c5f628
danielamoncada23 <danielamoncada@unah.hn>
2023-03-05 18:44:44 -0600

    pago a realizar

--- resources/views/tesoreria/paneltesoreria.blade.php
+++ resources/views/tesoreria/paneltesoreria.blade.php
@@ -6,7 +6,7 @@
       <div class="card">
           <center><img src="{{asset('img/brand/firma_contrato.jpg') }}" class="card-img-top" alt="..." style="width:127px;height:120px;"></center>
           <div class="card-body">
-              <center><a href="{{url('/usuarios/crear')}}" class="btn btn-lg btn-info">Firma Contrato</a></center>
+              <center><a href="{{Route('dashboard.index')}}" class="btn btn-lg btn-info">Firma Contrato</a></center>
           </div>
       </div>
   </div>
@@ -14,7 +14,7 @@
       <div class="card">
           <center><img src="{{asset('img/brand/pago_realizar.jpg') }}" class="card-img-top" alt="..." style="width:127px;height:120px;"></center>
           <div class="card-body">
-              <center><a href="{{Route('dashboard.index')}}" class="btn btn-lg btn-info">Pago a realizar</a></center>
+              <center><a href="{{Route('pagorealizar.index')}}" class="btn btn-lg btn-info">Pago a realizar</a></center>
           </div>
       </div>
   </div>
@@ -22,7 +22,7 @@
       <div class="card">
           <center><img src="{{asset('img/brand/pago_retrasada.jpg') }}" class="card-img-top" alt="..." style="width:120px;height:120px;"></center>
           <div class="card-body">
-              <center><a href="{{url('/usuarios/crear')}}" class="btn btn-lg btn-info">Pago Retrasada</a></center>
+              <center><a href="{{Route('dashboard.index')}}" class="btn btn-lg btn-info">Pago Retrasada</a></center>
           </div>
       </div>
   </div>
@@ -33,7 +33,7 @@
       <div class="card">
           <center><img src="{{asset('img/brand/n_recibo.jpg') }}" class="card-img-top" alt="..." style="width:120px;height:120px;"></center>
           <div class="card-body">
-              <center><a href="{{url('/usuarios/crear')}}" class="btn btn-lg btn-info">N-Recibo</a></center>
+              <center><a href="{{Route('dashboard.index')}}" class="btn btn-lg btn-info">N-Recibo</a></center>
           </div>
       </div>
   </div>
@@ -41,7 +41,7 @@
       <div class="card">
           <center><img src="{{asset('img/brand/reporte_diario.png') }}" class="card-img-top" alt="..." style="width:120px;height:120px;"></center>
           <div class="card-body">
-              <center><a href="{{url('/usuarios/crear')}}" class="btn btn-lg btn-info">Reporte diario</a></center>
+              <center><a href="{{Route('dashboard.index')}}" class="btn btn-lg btn-info">Reporte diario</a></center>
           </div>
       </div>
   </div>
@@ -49,7 +49,7 @@
       <div class="card">
           <center><img src="{{asset('img/brand/ingresos_totales.jpg') }}" class="card-img-top" alt="..." style="width:120px;height:120px;"></center>
           <div class="card-body">
-              <center><a href="{{url('/usuarios/crear')}}" class="btn btn-lg btn-info">Ingresos Totales</a></center>
+              <center><a href="{{Route('dashboard.index')}}" class="btn btn-lg btn-info">Ingresos Totales</a></center>
           </div>
       </div>
   </div>
