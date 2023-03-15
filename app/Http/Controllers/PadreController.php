01d5cd1a1ceaaf342936dc1353c4bfa418500f25
danielamoncada23 <danielamoncada@unah.hn>
2023-03-14 22:50:27 -0600

    matricula

--- app/Http/Controllers/PadreController.php
+++ app/Http/Controllers/PadreController.php
@@ -81,6 +81,152 @@ class PadreController extends Controller
         return redirect('/padres')->with('success', '¡El dato ha sido guardado/actualizado correctamente!');
     }
 
+      public function createdatospadre()
+      {
+        return view('secretaria.matricula.datospadre');
+      }
+
+      public function storeconpadre(Request $request)
+      {
+        $rules = [
+            'tipo' => 'required',
+            'primernombre' => 'required|alpha',
+            'segundonombre'=> 'required|alpha',
+            'primerapellido' => 'required|alpha',
+            'segundoapellido'=> 'required|alpha',
+            'numerodeidentidad'=> 'required|min:13|numeric',
+            'telefonopersonal'=> 'required|min:8|numeric',
+            'lugardetrabajo'=> 'required|alpha',
+            'oficio'=> 'required|alpha',
+            'telefonooficina'=> 'required|min:8|numeric',
+            'ingresos'=> 'required|numeric',
+        ];
+
+        $messages= [
+            'telefonopersonal.min' => 'El número de teléfono personal mínimo debe tener 8 dígitos',
+            'telefonopersonal.numeric' => 'El número de teléfono personal deben ser dígitos del 1 al 10',
+            'telefonooficina.min' => 'El número de teléfono de oficina mínimo debe tener 8 dígitos',
+            'telefonooficina.numeric' => 'El número de teléfono de oficina deben ser dígitos del 1 al 10',
+            'ingresos.numeric' => 'Los ingresos deben ser valores numéricos',
+            'numerodeidentidad.numeric' => 'El número de identidad deben tener solo valores numéricos',
+            'numerodeidentidad.min' => 'El número de identidad deben tener al menos 13 dígitos',
+            'primernombre.alpha' => 'El primer nombre no deben tener valores numéricos',
+            'segundonombre.alpha' => 'El segundo nombre no deben tener valores numéricos',
+            'primerapellido.alpha' => 'El primer apellido no deben tener valores numéricos',
+            'segundoapellido.alpha' => 'El segundo apellido no deben tener valores numéricos',
+            
+    
+           ];
+
+        $this->validate($request, $rules, $messages);
+
+        Padre::create(
+            $request->only('tipo','primernombre', 'segundonombre', 'primerapellido', 'segundoapellido',
+            'numerodeidentidad','telefonopersonal', 
+            'lugardetrabajo', 'oficio', 'telefonooficina', 'ingresos' )
+            );
+
+        return redirect('/alumnomadre');
+      }
+
+      public function createdatosmadre()
+      {
+        return view('secretaria.matricula.datosmadre');
+      }
+
+      public function storeconmadre(Request $request)
+      {
+        $rules = [
+            'tipo' => 'required',
+            'primernombre' => 'required|alpha',
+            'segundonombre'=> 'required|alpha',
+            'primerapellido' => 'required|alpha',
+            'segundoapellido'=> 'required|alpha',
+            'numerodeidentidad'=> 'required|min:13|numeric',
+            'telefonopersonal'=> 'required|min:8|numeric',
+            'lugardetrabajo'=> 'required|alpha',
+            'oficio'=> 'required|alpha',
+            'telefonooficina'=> 'required|min:8|numeric',
+            'ingresos'=> 'required|numeric',
+        ];
+
+        $messages= [
+            'telefonopersonal.min' => 'El número de teléfono personal mínimo debe tener 8 dígitos',
+            'telefonopersonal.numeric' => 'El número de teléfono personal deben ser dígitos del 1 al 10',
+            'telefonooficina.min' => 'El número de teléfono de oficina mínimo debe tener 8 dígitos',
+            'telefonooficina.numeric' => 'El número de teléfono de oficina deben ser dígitos del 1 al 10',
+            'ingresos.numeric' => 'Los ingresos deben ser valores numéricos',
+            'numerodeidentidad.numeric' => 'El número de identidad deben tener solo valores numéricos',
+            'numerodeidentidad.min' => 'El número de identidad deben tener al menos 13 dígitos',
+            'primernombre.alpha' => 'El primer nombre no deben tener valores numéricos',
+            'segundonombre.alpha' => 'El segundo nombre no deben tener valores numéricos',
+            'primerapellido.alpha' => 'El primer apellido no deben tener valores numéricos',
+            'segundoapellido.alpha' => 'El segundo apellido no deben tener valores numéricos',
+            
+    
+           ];
+
+        $this->validate($request, $rules, $messages);
+
+        Padre::create(
+            $request->only('tipo','primernombre', 'segundonombre', 'primerapellido', 'segundoapellido',
+            'numerodeidentidad','telefonopersonal', 
+            'lugardetrabajo', 'oficio', 'telefonooficina', 'ingresos' )
+            );
+
+        return redirect('/alumnoencargado');
+      }
+
+      public function createdatosencargado()
+      {
+        return view('secretaria.matricula.datosencargado');
+      }
+
+      public function storeconencargado(Request $request)
+      {
+        $rules = [
+            'tipo' => 'required',
+            'primernombre' => 'required|alpha',
+            'segundonombre'=> 'required|alpha',
+            'primerapellido' => 'required|alpha',
+            'segundoapellido'=> 'required|alpha',
+            'numerodeidentidad'=> 'required|min:13|numeric',
+            'telefonopersonal'=> 'required|min:8|numeric',
+            'lugardetrabajo'=> 'required|alpha',
+            'oficio'=> 'required|alpha',
+            'telefonooficina'=> 'required|min:8|numeric',
+            'ingresos'=> 'required|numeric',
+        ];
+
+        $messages= [
+            'telefonopersonal.min' => 'El número de teléfono personal mínimo debe tener 8 dígitos',
+            'telefonopersonal.numeric' => 'El número de teléfono personal deben ser dígitos del 1 al 10',
+            'telefonooficina.min' => 'El número de teléfono de oficina mínimo debe tener 8 dígitos',
+            'telefonooficina.numeric' => 'El número de teléfono de oficina deben ser dígitos del 1 al 10',
+            'ingresos.numeric' => 'Los ingresos deben ser valores numéricos',
+            'numerodeidentidad.numeric' => 'El número de identidad deben tener solo valores numéricos',
+            'numerodeidentidad.min' => 'El número de identidad deben tener al menos 13 dígitos',
+            'primernombre.alpha' => 'El primer nombre no deben tener valores numéricos',
+            'segundonombre.alpha' => 'El segundo nombre no deben tener valores numéricos',
+            'primerapellido.alpha' => 'El primer apellido no deben tener valores numéricos',
+            'segundoapellido.alpha' => 'El segundo apellido no deben tener valores numéricos',
+            
+    
+           ];
+
+        $this->validate($request, $rules, $messages);
+
+        Padre::create(
+            $request->only('tipo','primernombre', 'segundonombre', 'primerapellido', 'segundoapellido',
+            'numerodeidentidad','telefonopersonal', 
+            'lugardetrabajo', 'oficio', 'telefonooficina', 'ingresos' )
+            );
+
+        return redirect('/padres');
+      }
+
+
+
     /**
      * Display the specified resource.
      *
