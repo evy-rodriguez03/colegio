eab5cd1bbd6f9774de1070934fb353a5ce360a2f
evy-rodriguez03 <evyrodriguez03@gmail.com>
2023-04-18 22:24:00 -0600

    Verifiquen

--- app/Http/Controllers/requisitoController.php
+++ app/Http/Controllers/requisitoController.php
@@ -8,22 +8,22 @@ use Illuminate\Support\Facades\DB;
 
 class requisitoController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         $resultado = Alumno::select(
             DB::raw('(SELECT COUNT(id) FROM alumnos WHERE fotografias IS false) AS fotografiasa'),
             DB::raw('(SELECT COUNT(id) FROM alumnos WHERE fotografiasdelpadre IS false) AS fotografiasp'),
             DB::raw('(SELECT COUNT(id) FROM alumnos WHERE carnet IS false) AS carnet'),
             DB::raw('(SELECT COUNT(id) FROM alumnos WHERE certificadodeconducta IS false) AS certificado')
-        )->get();
+        )->first();
 
         $datos = [
-            'fotoa' => $resultado[0]->fotografiasa,
-            'fotop' => $resultado[0]->fotografiasp,
-            'carnet' => $resultado[0]->carnet,
-            'certi' => $resultado[0]->certificado,
+            'fotoa' => isset($resultado) ? $resultado->fotografiasa : 0,
+            'fotop' => isset($resultado) ? $resultado->fotografiasp : 0,
+            'carnet' => isset($resultado) ? $resultado->carnet : 0,
+            'certi' => isset($resultado) ? $resultado->certificado : 0,
         ];
         $jsonDatos = json_encode($datos);
         return view('secretaria/requisito', ['jsonDatos' => $jsonDatos ]);
-        
-       }
+    }
 }
