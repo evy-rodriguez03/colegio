ee1e00a33ae9c79a5292c9c11261cb179247a6e4
danielamoncada23 <danielamoncada@unah.hn>
2023-04-18 19:34:47 -0600

    panel orientacion

---
+++ resources/views/orientacion/panelorientacion.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.panel')
+
+@section('content')
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+      <div class="card">
+          <center><img src="{{asset('img/brand/preescolar.jpg') }}" class="card-img-top" alt="..." style="width:127px;height:120px;"></center>
+          <div class="card-body">
+              <center><a href="{{Route('dashboard.index')}}" class="btn btn-lg btn-info">Formulario Escolar</a></center>
+          </div>
+      </div>
+  </div>
+  <div class="col">
+      <div class="card">
+          <center><img src="{{asset('img/brand/preescolar.jpg') }}" class="card-img-top" alt="..." style="width:127px;height:120px;"></center>
+          <div class="card-body">
+              <center><a href="{{Route('pagorealizar.index')}}" class="btn btn-lg btn-info">Formulario preescolar</a></center>
+          </div>
+      </div>
+  </div>
+</div>
+  </div>
+</div>
+
+
+
+
+@endsection
