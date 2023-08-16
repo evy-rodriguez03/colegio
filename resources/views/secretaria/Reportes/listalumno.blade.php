bd0568a4538bb15a33589e60fbc6e021dba40f50
evy-rodriguez03 <evyrodriguez.@gmail.com>
2023-08-16 01:06:13 -0600

    Cambio de butones y correpciones de pdf

--- resources/views/secretaria/Reportes/listalumno.blade.php
+++ resources/views/secretaria/Reportes/listalumno.blade.php
@@ -4,59 +4,67 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title> PDF Reportes Alumnos</title>
     <style>
-        .cabecera{
-            color: black;}
+        .cabecera {
+            color: black;
+        }
 
-        .h1{
-            color: black; }
-            table {
-        border-collapse: collapse;
-        margin: auto;
-    }
-    td, th {
-        border: 1px solid black;
-        padding: 5px;
-    }
+        .h1 {
+            color: black;
+        }
 
+        table {
+            border-collapse: collapse;
+            margin: auto;
+        }
+
+        td, th {
+            border: 1px solid black;
+            padding: 5px;
+        }
     </style>
 </head>
 <body>
-<center><img src="{{asset('img/brand/blue.png') }}" class="card-img-top" alt="..." style="width:70px;height:70px;"></center>
-<center><h4>INSTITUTO "COSME GARCIA C".</h4>
-<h3>MATRICULA CORRESPONDIENTE AL AÑO {{ date('Y') }}</h3></center>  
-<table class="table table-bordered" style="text-align: center; font-size: 15px";> 
-        <thead class="cabecera">
-          <tr>
-            <th scope="row">N°</th>
-            <th scope="row">Nombre Del Alumno</th>
-            <th scope="row">N° de Registro</th>
-            <th scope="row">Fecha De Nacimiento</th>
-            <th scope="row">Edad</th>
-            <th scope="row">Instituto De Procedencia</th>
-            <th scope="row">Padre o Encargado</th>
-            <th scope="row">Dirección</th>
-            <th scope="row">Fecha de Mátricula</th>
-          </tr>
-        </thead>
-        <tbody>
-        @foreach ($alumnos as $index => $alumno)
-      <tr>
-        <td>{{ $index + 1 }}</td>
-        <td>{{ $alumno->primernombre }} {{ $alumno->segundonombre }} {{ $alumno->primerapellido }} {{ $alumno->segundoapellido }}</td>
-        <td>{{ $alumno->numerodeidentidad }}</td>
-        <td>{{ $alumno->fechadenacimiento }}</td>
-        <td>{{$alumno->escuelaanterior}}</td>
-        <td>
-          @foreach($padres as $padre)
-            @if($padre->id == $alumno->padre_id)
-              {{$padre->primernombre}} {{$padre->primerapellido}}
-            @endif
-          @endforeach
-        </td>
-        <td>{{$alumno->direccion}}</td>
-      </tr>
-      @endforeach
-        </tbody>
-      </table>
+<center><img src="{{ asset('img/brand/blue.png') }}" class="card-img-top" alt="..."
+             style="width:70px;height:70px;"></center>
+<center>
+    <h4>INSTITUTO "COSME GARCIA C".</h4>
+    <h3>MATRICULA CORRESPONDIENTE AL AÑO {{ date('Y') }}</h3>
+</center>
+<table class="table table-bordered" style="text-align: center; font-size: 15px;">
+    <thead class="cabecera">
+    <tr>
+        <th scope="row">N°</th>
+        <th scope="row">Nombre Del Alumno</th>
+        <th scope="row">N° de Registro</th>
+        <th scope="row">Fecha De Nacimiento</th>
+        <th scope="row">Edad</th>
+        <th scope="row">Instituto De Procedencia</th>
+        <th scope="row">Padre o Encargado</th>
+        <th scope="row">Dirección</th>
+        <th scope="row">Fecha de Mátricula</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach ($alumnos as $index => $alumno)
+        <tr>
+            <td>{{ $index + 1 }}</td>
+            <td>{{ $alumno->primernombre }} {{ $alumno->segundonombre }} {{ $alumno->primerapellido }} {{ $alumno->segundoapellido }}</td>
+            <td>{{ $alumno->numerodeidentidad }}</td>
+            <td>{{ $alumno->fechadenacimiento }}</td>
+            <td>{{ \Carbon\Carbon::parse($alumno->fechadenacimiento)->diff(\Carbon\Carbon::now())->format('%y años') }}</td>
+            <td>{{ $alumno->escuelaanterior }}</td>
+            <td>
+                @foreach($padres as $padre)
+                    @if($padre->id == $alumno->padre_id)
+                        {{ $padre->primernombre }} {{ $padre->primerapellido }}
+                    @endif
+                @endforeach
+            </td>
+            <td>{{ $alumno->direccion }}</td>
+            <td>{{ \Carbon\Carbon::now()->format('d/m/Y') }}</td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
 </body>
 </html>
--- resources/views/secretaria/Reportes/listapadre.blade.php
+++ resources/views/secretaria/Reportes/listapadre.blade.php
@@ -2,61 +2,68 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title> PDF Reportes Alumnos</title>
+    <title> PDF Reportes Padres</title>
     <style>
-        .cabecera{
-            color: black;}
+        .cabecera {
+            color: black;
+        }
 
-        .h1{
-            color: black; }
-            table {
-        border-collapse: collapse;
-        margin: auto;
-    }
-    td, th {
-        border: 1px solid black;
-        padding: 5px;
-    }
+        .h1 {
+            color: black;
+        }
+
+        table {
+            border-collapse: collapse;
+            margin: auto;
+        }
+
+        td, th {
+            border: 1px solid black;
+            padding: 5px;
+        }
 
     </style>
 </head>
 <body>
-<center><img src="{{asset('img/brand/blue.png') }}" class="card-img-top" alt="..." style="width:70px;height:70px;"></center>
-<center><h4>INSTITUTO "COSME GARCIA C".</h4>
-<h3>MATRICULA CORRESPONDIENTE AL AÑO {{ date('Y') }}</h3></center>  
-<table class="table table-bordered" style="text-align: center; font-size: 15px";> 
-        <thead class="cabecera">
-          <tr>
-            <th scope="row">N°</th>
-            <th scope="row">Nombre Del Alumno</th>
-            <th scope="row">Padre o Encargado</th>
-            <th scope="row">Parentesco</th>
-            <th scope="row">Fecha de Nacimiento</th>
-            <th scope="row">N° de Identidad</th>
-            <th scope="row">N° de Telefono</th>
-            <th scope="row">Dirección</th>
-            <th scope="row">Firma</th>
-            <th scope="row">Curso</th>
-          </tr>
-        </thead>
-        <tbody>
-        @foreach ($alumnos as $index => $alumno)
-      <tr>
-        <td>{{ $index + 0 }}</td>
-        <td>{{ $alumno->primernombre }} {{ $alumno->segundonombre }} {{ $alumno->primerapellido }} {{ $alumno->segundoapellido }}</td>
-        <td>
-          @foreach($padres as $padre)
-          <td>{{$padre->primernombre}} {{$padre->primerapellido}}</td>
-          <td>{{$padre->tipo}}</td>
-          <td>{{$padre->direccion}}</td>
-          <td>{{$padre->numerodeidentidad}}</td>
-          <td>{{$padre->telefonopersonal}}</td>
-          @endforeach
-        </td>
-        
-      </tr>
-      @endforeach
-        </tbody>
-      </table>
+<center><img src="{{ asset('img/brand/blue.png') }}" class="card-img-top" alt="..."
+             style="width:70px;height:70px;"></center>
+<center>
+    <h4>INSTITUTO "COSME GARCIA C".</h4>
+    <h3>MATRICULA CORRESPONDIENTE AL AÑO {{ date('Y') }}</h3>
+</center>
+<table class="table table-bordered" style="text-align: center; font-size: 15px;">
+    <thead class="cabecera">
+    <tr>
+        <th scope="row">N°</th>
+        <th scope="row">Nombre Del Alumno</th>
+        <th scope="row">Padre o Encargado</th>
+        <th scope="row">Parentesco</th>
+        <th scope="row">Fecha de Nacimiento</th>
+        <th scope="row">N° de Identidad</th>
+        <th scope="row">N° de Telefono</th>
+        <th scope="row">Dirección</th>
+        <th scope="row">Firma</th>
+        <th scope="row">Curso</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach ($alumnos as $index => $alumno)
+        <tr>
+            <td>{{ $index + 1 }}</td>
+            <td>{{ $alumno->primernombre }} {{ $alumno->segundonombre }} {{ $alumno->primerapellido }} {{ $alumno->segundoapellido }}</td>
+            @foreach($padres as $padre)
+                <td>{{$padre->primernombre}} {{$padre->primerapellido}}</td>
+                <td>{{$padre->tipo}}</td>
+                <td></td> <!-- Celdas vacías para las columnas de Fecha de Nacimiento, N° de Identidad y N° de Telefono -->
+                <td></td>
+                <td></td>
+                <td>{{$padre->direccion}}</td>
+                <td></td> <!-- Celda vacía para la columna "Firma" -->
+                <td></td> <!-- Celda vacía para la columna Curso -->
+            @endforeach
+        </tr>
+    @endforeach
+    </tbody>
+</table>
 </body>
 </html>
