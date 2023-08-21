c30a5e1155ccbe670802f6b48af7805deee18bcd
evy-rodriguez03 <evyrodriguez.@gmail.com>
2023-08-20 23:12:54 -0600

    Permisos de secretaria terminados.

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -22,20 +22,36 @@ class RoleSeeder extends Seeder
         $role4 = Role::create(['name' => 'Orientacion']);
         $role5 = Role::create(['name' => 'Consejeria']);
 
-        //Permisos para admin 
+        //Asignacion de permisos 
         $permission = Permission::create(['name' => 'dashboard.index'])->syncRoles([$role1, $role2, $role3, $role4, $role5 ]);
-        $permission = Permission::create(['name' => 'dashboardsec.index'])->assignRole($role2);
+        $permission = Permission::create(['name' => 'dashboardsec.index'])->assignRole($role1, $role2);
         $permission = Permission::create(['name' => 'paneltesoreria.index'])->assignRole($role3);
+        
+        //asignacion_de_usuario
         $permission = Permission::create(['name' => 'usuarios.index'])->assignRole($role1);
         $permission = Permission::create(['name' => 'usuarios.create'])->assignRole($role1);
         $permission = Permission::create(['name' => 'usuarios.edit'])->assignRole($role1);
         $permission = Permission::create(['name' => 'usuarios.update'])->assignRole($role1);
         $permission = Permission::create(['name' => 'usuarios.destroy'])->assignRole($role1);
+        $permission = Permission::create(['name' => 'sendData'])->assignRole($role1);
+        $permission = Permission::create(['name' => 'usuarios.deshabilitar'])->assignRole($role1);
+        $permission = Permission::create(['name' => 'usuarios.habilitar'])->assignRole($role1);
+        
+        //reportes_matricula
+        $permission = Permission::create(['name' => 'reportes.index'])->assignRole($role1, $role2);
+        $permission = Permission::create(['name' => 'repalumno.pdf'])->assignRole($role1, $role2);
+        $permission = Permission::create(['name' => 'repadre.pdf'])->assignRole($role1, $role2);
+
+        //periodo_matricula
         $permission = Permission::create(['name' => 'periodo'])->assignRole($role1);
         $permission = Permission::create(['name' => 'inicio.create'])->assignRole($role1);
         $permission = Permission::create(['name' => 'inicio.store'])->assignRole($role1);
+        
+        //cursos
         $permission = Permission::create(['name' => 'cursos.pdf'])->syncRoles([$role1, $role2]);
         $permission = Permission::create(['name' => 'cursos'])->syncRoles([$role1, $role2]);
+        
+        //padres
         $permission = Permission::create(['name' => 'padre.pdf'])->syncRoles([$role1, $role2]);
         $permission = Permission::create(['name' => 'padres.index'])->syncRoles([$role1, $role2]);
         $permission = Permission::create(['name' => 'padres.create'])->syncRoles([$role1, $role2]);
@@ -43,6 +59,8 @@ class RoleSeeder extends Seeder
         $permission = Permission::create(['name' => 'padres.update'])->syncRoles([$role1, $role2]);
         $permission = Permission::create(['name' => 'padres.destroy'])->syncRoles([$role1, $role2]);
         $permission = Permission::create(['name' => 'padre.show'])->syncRoles([$role1, $role2]);
+        
+        //alumnos
         $permission = Permission::create(['name' => 'alumnos.index'])->syncRoles([$role1, $role2]);
         $permission = Permission::create(['name' => 'alumnos.create'])->syncRoles([$role1, $role2]);
         $permission = Permission::create(['name' => 'alumnos.edit'])->syncRoles([$role1, $role2]);
@@ -51,16 +69,34 @@ class RoleSeeder extends Seeder
         $permission = Permission::create(['name' => 'alumnos.show'])->syncRoles([$role1, $role2]);
         $permission = Permission::create(['name' => 'alumnos.destroy'])->syncRoles([$role1, $role2]);
         $permission = Permission::create(['name' => 'retrasadas.index'])->syncRoles([$role1, $role2]);
+        
+        //perfil
         $permission = Permission::create(['name' => 'profile'])->syncRoles([$role1, $role2, $role3, $role4, $role5 ]);
         $permission = Permission::create(['name' => 'profile.edit'])->syncRoles([$role1, $role2, $role3, $role4, $role5 ]);
         $permission = Permission::create(['name' => 'profile.update'])->syncRoles([$role1, $role2, $role3, $role4, $role5 ]);
         $permission = Permission::create(['name' => 'profile.editar'])->syncRoles([$role1, $role2, $role3, $role4, $role5 ]);
+       
+        //imagen_perfil       
         $permission = Permission::create(['name' => 'imagenE.index'])->syncRoles([$role1, $role2, $role3, $role4, $role5 ]);
         $permission = Permission::create(['name' => 'image.store'])->syncRoles([$role1, $role2, $role3, $role4, $role5 ]);
-        $permission = Permission::create(['name' => 'horario.index'])->assignRole($role1);
-        $permission = Permission::create(['name' => 'horario.create'])->assignRole($role1);
+        $permission = Permission::create(['name' => 'imagenE.destroy'])->syncRoles([$role1, $role2, $role3, $role4, $role5 ]);
         
-        //Permisos para secretaria
+        //horario
+        $permission = Permission::create(['name' => 'horario.index'])->assignRole($role1, $role2);
+        $permission = Permission::create(['name' => 'horario.create'])->assignRole($role1, $role2);
+        
+       //matriculado 
+       $permission = Permission::create(['name' => 'creatematricula'])->assignRole($role1, $role2);
+       $permission = Permission::create(['name' => 'submitmatricula'])->assignRole($role1, $role2);
+       $permission = Permission::create(['name' => 'datospadre.create'])->assignRole($role1, $role2);
+       $permission = Permission::create(['name' => 'submitpadre'])->assignRole($role1, $role2);
+       $permission = Permission::create(['name' => 'datosmadre.create'])->assignRole($role1, $role2);
+       $permission = Permission::create(['name' => 'datosencargado.create'])->assignRole($role1, $role2);
+       $permission = Permission::create(['name' => 'submitencargado'])->assignRole($role1, $role2);
+       $permission = Permission::create(['name' => 'parientetransporte'])->assignRole($role1, $role2);
+       $permission = Permission::create(['name' => 'terminar_matricula'])->assignRole($role1, $role2);
+       
+
 
      
 
